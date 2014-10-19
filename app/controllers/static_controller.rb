bc5abf5f6df2be7291b5b69cd890340a33e4f66c
Ricardo Canales <canalesb93@gmail.com>
2014-10-19 08:05:41 -0500

    Various events

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,14 +1,15 @@
 class StaticController < ApplicationController
   def home
     @location = lookup_ip_location.first
-    
     @events = Event.near(@location.coordinates, 20)
-    @events = @events.order('(select count(posts.id) from posts where posts.event_id = events.id)')
-    @event = @events.first
-    @posts = @event.posts.order("created_at DESC")
-    @topposts = @event.posts.where("score > ?", 3).limit(3).order("score DESC")
-    @post = Post.new
-    @user = current_user
+    if !@events.empty?
+      @events = @events.order('(select count(posts.id) from posts where posts.event_id = events.id)')
+      @event = @events.first
+      @posts = @event.posts.order("created_at DESC")
+      @topposts = @event.posts.where("score > ?", 3).limit(3).order("score DESC")
+      @post = Post.new
+      @user = current_user
+    end
   end
 
   def about
