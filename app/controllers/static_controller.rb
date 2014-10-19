d0bd21b5b44c4fcd4d656d8950e4eb8aa95bf57c
Ricardo Canales <canalesb93@gmail.com>
2014-10-19 06:46:51 -0500

    Order by count

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,6 +3,7 @@ class StaticController < ApplicationController
     @location = lookup_ip_location.first
 
     @events = Event.near(@location.coordinates, 20)
+    @events = @events.order('(select count(posts.id) from posts where posts.event_id = events.id)')
     @event = @events.first
     @posts = @event.posts.order("created_at DESC")
     @topposts = @event.posts.where("score > ?", 3).limit(3).order("score DESC")
